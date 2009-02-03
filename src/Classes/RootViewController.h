c8a964ef193eb4b7674d34e31a6e7b57596be7c3
Doug Kurth <doug@highorderbit.com>
2009-02-03 15:38:58 -0700

    Changed to meet coding conventions

--- src/Classes/RootViewController.h
+++ src/Classes/RootViewController.h
@@ -1,14 +1,11 @@
 //
-//  RootViewController.h
-//  build-watch
-//
-//  Created by Douglas Kurth on 2/3/09.
 //  Copyright High Order Bit, Inc. 2009. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
-@interface RootViewController : UITableViewController {
+@interface RootViewController : UITableViewController
+{
 }
 
 @end
--- src/Classes/RootViewController.m
+++ src/Classes/RootViewController.m
@@ -1,129 +1,66 @@
 //
-//  RootViewController.m
-//  build-watch
-//
-//  Created by Douglas Kurth on 2/3/09.
 //  Copyright High Order Bit, Inc. 2009. All rights reserved.
 //
 
 #import "RootViewController.h"
-#import "build_watchAppDelegate.h"
-
+#import "BuildWatchAppDelegate.h"
 
 @implementation RootViewController
 
-- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+- (NSInteger) numberOfSectionsInTableView:(UITableView *)tableView
+{
     return 1;
 }
 
-
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+- (NSInteger) tableView:(UITableView *)tableView
+  numberOfRowsInSection:(NSInteger)section
+{
     return 0;
 }
 
-
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+- (UITableViewCell *) tableView:(UITableView *)tableView
+          cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
     
     static NSString *CellIdentifier = @"Cell";
     
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
-    if (cell == nil) {
-        cell = [[[UITableViewCell alloc] initWithFrame:CGRectZero reuseIdentifier:CellIdentifier] autorelease];
-    }
+    UITableViewCell *cell =
+        [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    
+    if (cell == nil)
+        cell =
+            [[[UITableViewCell alloc]
+              initWithFrame:CGRectZero reuseIdentifier:CellIdentifier]
+             autorelease];
     
     // Set up the cell
     return cell;
 }
 
-
-- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+- (void)      tableView:(UITableView *)tableView
+didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
     // Navigation logic -- create and push a new view controller
 }
 
-
-/*
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Uncomment the following line to add the Edit button to the navigation bar.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
-}
-*/
-
-
-/*
-// Override to support editing the list
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
-    
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source
-        [tableView deleteRowsAtIndexPaths:[NSArray arrayWithObject:indexPath] withRowAnimation:YES];
-    }   
-    if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
-}
-*/
-
-
-/*
-// Override to support conditional editing of the list
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
-}
-*/
-
-
-/*
-// Override to support rearranging the list
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
-}
-*/
-
-
-/*
-// Override to support conditional rearranging of the list
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
-}
-*/
-
-/*
-- (void)viewWillAppear:(BOOL)animated {
-    [super viewWillAppear:animated];
-}
-*/
-/*
-- (void)viewDidAppear:(BOOL)animated {
-    [super viewDidAppear:animated];
-}
-*/
-/*
-- (void)viewWillDisappear:(BOOL)animated {
-}
-*/
-/*
-- (void)viewDidDisappear:(BOOL)animated {
-}
-*/
-
-- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
+- (BOOL) shouldAutorotateToInterfaceOrientation:
+    (UIInterfaceOrientation)interfaceOrientation
+{
     // Return YES for supported orientations
     return (interfaceOrientation == UIInterfaceOrientationPortrait);
 }
 
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning]; // Releases the view if it doesn't have a superview
+- (void) didReceiveMemoryWarning
+{
+    // Releases the view if it doesn't have a superview
+    [super didReceiveMemoryWarning];
+    
     // Release anything that's not essential, such as cached data
 }
 
-
-- (void)dealloc {
+- (void) dealloc
+{
     [super dealloc];
 }
 
-
 @end
-
