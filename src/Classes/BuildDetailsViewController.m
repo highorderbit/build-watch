27a6bbd2bbf6ced4cfd34e1c8080f217bbe15f8f
John A. Debay <john@highorderbit.com>
2009-02-10 20:48:31 -0700

    Should have been part of last commit. Using real values for build details. Showing full build details in a disclosure window. Needs to be refactored to route through the app controller so it fits with the rest of the drill down structure.

---
+++ src/Classes/BuildDetailsViewController.m
@@ -0,0 +1,52 @@
+//
+//  Copyright 2009 High Order Bit, Inc.. All rights reserved.
+//
+
+#import "BuildDetailsViewController.h"
+
+@implementation BuildDetailsViewController
+
+@synthesize headerLabel;
+@synthesize headerImage;
+@synthesize webView;
+@synthesize delegate;
+@synthesize projectId;
+
+- (void) dealloc
+{
+    [headerLabel release];
+    [headerImage release];
+    [webView release];
+    [delegate release];
+    [projectId release];
+    [super dealloc];
+}
+
+- (void) viewDidLoad
+{
+    [super viewDidLoad];
+}
+
+- (void) viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+
+    NSString * displayName = [delegate displayNameForProject:projectId];
+    NSString * label = [delegate labelForProject:projectId];
+    NSString * description = [delegate descriptionForProject:projectId];
+
+    self.navigationItem.title =
+        [NSString stringWithFormat:
+         NSLocalizedString(@"builddetails.title.formatstring", @""),
+         displayName, label];
+
+    NSString * html = [NSString stringWithFormat:
+        @"<html><head><title>%@</title></head>"
+        "<body style=\"font-family: 'Lucida Grande'; font-size: 3em; margin: 30px;\">%@</body>"
+         "</body></html>",
+        displayName, description];
+
+    [webView loadHTMLString:html baseURL:[NSURL URLWithString:@"/"]];
+}
+
+@end
