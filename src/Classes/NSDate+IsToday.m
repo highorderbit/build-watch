46908cb2d5289b2d999607653b936dc86724f786
Doug Kurth <doug@highorderbit.com>
2009-02-19 14:02:41 -0700

    Fixed day-of-week for recent date formatting post case

--- src/Classes/NSDate+IsToday.m
+++ src/Classes/NSDate+IsToday.m
@@ -13,22 +13,37 @@
     unsigned unitFlags =
         NSYearCalendarUnit | NSMonthCalendarUnit |  NSDayCalendarUnit;
     
-    NSDateComponents * selfComponents =
+    NSDateComponents * selfComps =
         [currentCalendar components:unitFlags fromDate:self];
     
     NSDate * now = [NSDate date];
 
-    NSDateComponents * nowComponents =
+    NSDateComponents * nowComps =
         [currentCalendar components:unitFlags fromDate:now];
     
-    return [nowComponents day] == [selfComponents day] &&
-        [nowComponents month] == [selfComponents month] &&
-        [nowComponents year] == [selfComponents year];
+    return [nowComps day] == [selfComps day] &&
+        [nowComps month] == [selfComps month] &&
+        [nowComps year] == [selfComps year];
 }
 
 - (BOOL) isLessThanWeekAgo
 {
-    return -[self timeIntervalSinceNow] < 60 * 60 * 24 * 6;
+    NSCalendar * currentCalendar = [NSCalendar currentCalendar];
+
+    unsigned unitFlags =
+    NSYearCalendarUnit | NSMonthCalendarUnit |  NSDayCalendarUnit;
+    
+    NSDateComponents * selfComps =
+        [currentCalendar components:unitFlags fromDate:self];
+    
+    NSDateComponents * comps = [[[NSDateComponents alloc] init] autorelease];
+    [comps setDay:[selfComps day]];
+    [comps setMonth:[selfComps month]];
+    [comps setYear:[selfComps year]];
+    
+    NSDate * beginningOfToday = [currentCalendar dateFromComponents:comps];
+
+    return -[beginningOfToday timeIntervalSinceNow] < 60 * 60 * 24 * 7;
 }
 
 @end
